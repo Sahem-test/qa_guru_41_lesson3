19d5d2d093b969c61a32e8aefb838edcdec5e96c
Admin <dir.gefest@gmail.com>
2026-03-28 09:57:51 +0300

    add JUnitExamplesTest

---
+++ src/test/java/tests/examples/JUnitExamplesTest.java
@@ -0,0 +1,35 @@
+package tests.examples;
+
+import org.junit.jupiter.api.*;
+
+public class JUnitExamplesTest {
+    @BeforeAll
+    static void startUp() {
+        System.out.println("Start UP");
+    }
+
+    @BeforeEach
+    void startBrowser() {
+        System.out.println("Start browser");
+    }
+
+    @Test
+    void firstTest() {
+        System.out.println(" - firstTest");
+    }
+
+    @Test
+    void secondTest() {
+        System.out.println(" - secondTest");
+    }
+
+    @AfterEach
+    void stopBrowser() {
+        System.out.println("Stop browser");
+    }
+
+    @AfterAll
+    static void finishDown() {
+        System.out.println("Finish Down UP");
+    }
+}
